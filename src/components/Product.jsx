775a509f7f4a4d38e566118bbc632cedeb6f2c70
Mokhtar Ouidah <mokhtarouidah@Mokhtars-iMac.local>
2023-10-26 18:57:20 -0400

    contest

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,14 +1,20 @@
-import React, { useState, useEffect } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import { useParams } from "react-router";
 import { NavLink } from "react-router-dom";
 import Skeleton from "react-loading-skeleton";
+import { CartContext } from '../context/cart'
+import Cart from './Cart'
 
 const Product = () => {
+  const { cartItems, addToCart } = useContext(CartContext)
+  const [showModal, setshowModal] = useState(false);
   const { id } = useParams();
   const [product, setProduct] = useState([]);
   const [loading, setLoading] = useState(false);
 
-  
+  const toggle = () => {
+    setshowModal(!showModal);
+  };
   
   // Use addToCart instead of addProduct
   
@@ -53,6 +59,7 @@ const Product = () => {
           />
         </div>
         <div className="col-md-6">
+        
           <h4 className="text-uppercase text-black-50">{product.category}</h4>
           <h1 className="display-5">{product.title}</h1>
           <p className="lead fw-bolder">
@@ -63,7 +70,7 @@ const Product = () => {
           <p className="lead">{product.description}</p>
           
           {/* Use addToCart instead of addProduct */}
-          <button className="btn btn-outline-dark px-4 py-2" onClick={() => console.log(product)}>
+          <button className="btn btn-outline-dark px-4 py-2"onClick={() => addToCart(product)}>
             Add to Cart
           </button>
           <NavLink to="/cart" className="btn btn-dark ms-2 px-3 py-2">
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,7 +1,10 @@
-import React, { useState, useEffect } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import Skeleton from "react-loading-skeleton";
 import { NavLink } from "react-router-dom";
+import { CartContext } from '../context/cart'
+import Cart from './Cart'
 const Products = () => {
+  const { cartItems, addToCart } = useContext(CartContext)
   const [data, setData] = useState([]);
   const [filter, setFilter] = useState(data);
   const [loading, setLoading] = useState(false);
